b58d1aed075ba3367e4d57261ddbd2096e4e2184
Peppe289 <gsperanza204@gmail.com>
2021-01-06 18:22:16 +0100

    change for begonia

--- anykernel.sh
+++ anykernel.sh
@@ -1,20 +1,27 @@
+ 
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # begin properties
 properties() { '
+kernel.string=Rave Kernel
 do.devicecheck=1
 do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lavender
-supported.versions=9 - 11
+device.name1=begonia
+device.name2=begoniain
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/platform/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -25,15 +32,16 @@ ramdisk_compression=auto;
 
 
 ## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
-
-# end ramdisk changes
 
 write_boot;
 ## end install
+
+
