9c13e4a6ad6e0277c786d3d6801742856b292848
Peppe289 <69747171+Peppe289@users.noreply.github.com>
2021-02-11 14:55:30 +0100

    change for platina

--- anykernel.sh
+++ anykernel.sh
@@ -6,10 +6,15 @@
 properties() { '
 do.devicecheck=1
 do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lavender
-supported.versions=9 - 11
+device.name1=platina
+device.name2=MI8Lite
+device.name3=mi8lite
+device.name4=8lite
+device.name5=
+supported.versions=
 supported.patchlevels=
 '; } # end properties
 
@@ -25,15 +30,14 @@ ramdisk_compression=auto;
 
 
 ## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
-
-# end ramdisk changes
-
 write_boot;
 ## end install
+
